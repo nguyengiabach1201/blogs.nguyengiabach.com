8526699bb40dfc4228e3141c9abdd49eab3ce7a5
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2025-05-09 17:45:05 +0700

    Reworked

--- src/config.ts
+++ src/config.ts
@@ -1,59 +1,22 @@
-import type { Site, SocialObjects } from "./types";
-
-export const SITE: Site = {
-  website: "https://blogs.nguyengiabach.com/",
+export const SITE = {
+  website: "https://blogs.nguyengiabach.com/", // replace this with your deployed domain
   author: "Nguyễn Gia Bách",
   profile: "https://nguyengiabach.com/",
   desc: "Welcome to my blog",
   title: "Blogs",
   ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
+  lightAndDarkMode: false,
   postPerIndex: 4,
-  postPerPage: 3,
+  postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
+  showBackButton: true, // show back button in post detail
   editPost: {
-    url: "https://github.com/nguyengiabach1201/blogs/tree/main/src/content/blog",
+    enabled: true,
     text: "Suggest Changes",
-    appendFilePath: true,
+    url: "https://github.com/nguyengiabach1201/blogs/edit/main/",
   },
-};
-
-export const LOCALE = {
+  dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
-
-export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
-};
-
-export const SOCIALS: SocialObjects = [
-  {
-    name: "Github",
-    href: "https://github.com/nguyengiabach1201",
-    linkTitle: `Me on Github`,
-    active: true,
-  },
-  {
-    name: "Mail",
-    href: "mailto:nguyengiabach1201@gmail.com",
-    linkTitle: `Send an email to me`,
-    active: true,
-  },
-  {
-    name: "Facebook",
-    href: "https://www.facebook.com/nguyengiabach1201/",
-    linkTitle: `Me on Facebook`,
-    active: true,
-  },
-  {
-    name: "X",
-    href: "https://x.com/giabach1201",
-    linkTitle: `Me on X`,
-    active: true,
-  },
-];
